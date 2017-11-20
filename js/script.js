c3c03c0d51640b9cb1665fc56d01fd50201a31b2
Raul Olmedo <raul.olmedo.dev@gmail.com>
2017-11-21 00:22:51 +0100

    Added basketball cards

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 function toggleView (event) {
+    /* Query the active classes involving the action and remove it */
     $('.nav-link.active').first().removeClass("active");
     $('.container.col-sm-12.active').first().removeClass("active");
 
+    /* Add the active class to the link clicked and activate the view of the desired tab */
     event.currentTarget.classList.add("active");
     $(''+ event.currentTarget.hash).addClass("active");
 
