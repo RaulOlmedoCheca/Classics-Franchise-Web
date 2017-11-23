b25e426e42c1e922c977f2ae4f0be8813d133e06
Raul Olmedo <raul.olmedo.dev@gmail.com>
2017-11-23 23:02:16 +0100

    Change bootstrap to materialize

--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
-function toggleView (event) {
-    /* Query the active classes involving the action and remove it */
-    $('.nav-link.active').first().removeClass("active");
-    $('.container.col-sm-12.active').first().removeClass("active");
-
-    /* Add the active class to the link clicked and activate the view of the desired tab */
-    event.currentTarget.classList.add("active");
-    $(''+ event.currentTarget.hash).addClass("active");
-
-}
+document.getElementsByClassName("sidenav").sidenav();
