ccce56b0e7bcb0440fb72e56845d321467739297
Raul Olmedo <raul.olmedo.dev@gmail.com>
2017-12-15 00:07:09 +0100

    Enesimo cambio de framework...

--- js/index.js
+++ js/index.js
@@ -1,20 +1,8 @@
-$(document).ready(function () {
-  $('.target').pushpin();
-});
-var elem = document.querySelector('.parallax');
-var instance1 = new M.Parallax(elem, options);
-var elem = document.querySelector('.scrollspy');
-var instance2 = new M.ScrollSpy(elem, options);
+(function($){
+  $(function(){
 
-$('.pushpin-demo-nav').each(function () {
-  var $this = $(this);
-  var $target = $('#' + $(this).attr('data-target'));
-  $this.pushpin({
-    top: $target.offset().top,
-    bottom: $target.offset().top + $target.outerHeight() - $this.height()
-  });
-});
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
 
-// Get the project instance (for curiosity purposes)
-// var instance1 = M.Parallax.getInstance(elem);
-// var instance2 = M.ScrollSpy.getInstance(elem);
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
