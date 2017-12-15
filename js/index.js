e0dced6ba600dd9906efc95303bf43ef837f6098
Raul Olmedo <raul.olmedo.dev@gmail.com>
2017-12-15 18:11:32 +0100

    Added basketball css and js + Cards with information

--- js/index.js
+++ js/index.js
@@ -4,4 +4,5 @@ $(document).ready(function () {
   $('.scrollspy').scrollSpy();
 });
 
+M.ScrollSpy.getInstance(elem);
 //apply active class and <li style="left: 0px; right: 768px;" class="indicator"></li>
